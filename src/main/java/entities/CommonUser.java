8886f15f01ace0fedfecca22d9fde9e895747c0f
Pooja Mangra <poojamangra@gmail.com>
2024-11-08 10:47:13 -0500

    Created entity package and data_access package. Created user interface and commonuser class.

---
+++ src/main/java/entities/CommonUser.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class CommonUser implements User {
+
+    private final String username;
+    private final String password;
+    private final String email;
+
+    public CommonUser(String username, String password, String email) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() { return username; }
+
+    @Override
+    public String getPassword() { return password; }
+
+    @Override
+    public String getEmail() { return email; }
+
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,24 @@
+package entities;
+
+/**
+ * Interface that represents a user.
+ */
+public interface User {
+    /**
+     * Returns the username of the user.
+     * @return username
+     */
+    String getUsername();
+
+    /**
+     * Return the password of the user
+     * @return password
+     */
+    String getPassword();
+
+    /**
+     * Return the email of the given user.
+     * @return email
+     */
+    String getEmail();
+}
