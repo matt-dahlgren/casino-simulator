f85818347fb479020cc58d3dacce6c6e06f5adc7
matt-dahlgren <matthew.dahlgren@bell.net>
2024-11-09 17:56:10 -0500

    Adding the interface and Class to interact with the Deck of Cards API

---
+++ src/main/java/api/CardDeck.java
@@ -0,0 +1,23 @@
+package api;
+
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import entities.Card;
+import entities.Dealer;
+import entities.Player;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * The CardDeck class used to interact with the DeckofCards API.
+ */
+public class CardDeck implements DeckofCards {
+
+}
