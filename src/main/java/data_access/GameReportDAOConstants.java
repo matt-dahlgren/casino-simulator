d44264f120eaf0c9ef498cbc1a8dcd4170385a7d
Jacob <jacob.lisogurski@mail.utoronto.ca>
2024-11-18 16:37:53 -0500

    DAO for the game summary use case, that retrieves a user's game history from a file. Also, one unit test.

---
+++ src/main/java/data_access/GameReportDAOConstants.java
@@ -0,0 +1,13 @@
+package data_access;
+
+/**
+ * Constants associated with game data.
+ */
+public class GameReportDAOConstants {
+    public static final int MOVE = 0;
+    public static final int WIN_PROBABILITY = 1;
+    public static final int ALTERNATE_WIN_PROBABILITY = 2;
+    public static final int PROBABILITY_DIFFERENCE = 3;
+    public static final int MOVE_QUALITY = 4;
+    public static final int NUM_STATISTICS = 5;
+}
