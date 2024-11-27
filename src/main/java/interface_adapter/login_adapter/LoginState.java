abc96503481c0e5fdd6f3b477d905ebb4eecb0b0
atStarling <andriy@shkvorets.ca>
2024-11-27 16:55:30 -0500

    Login Interface Adapters

--- src/main/java/interface_adapter/login_adapter/LoginState.java
+++ src/main/java/interface_adapter/login_adapter/LoginState.java
@@ -5,8 +5,21 @@ package interface_adapter.login_adapter;
  *
  */
 public class LoginState {
-    @Override
-    public String toString() {
-        return "LoginState";
+    private String username = "";
+    private String loginError;
+    private String password = "";
+
+    public String getUsername() {return username;}
+
+    public String getLoginError() {return loginError;}
+
+    public String getPassword() {return password;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public void setLoginError(String usernameError) {
+        this.loginError = usernameError;}
+
+    public void setPassword(String password) {this.password = password;}
+
     }
-}
