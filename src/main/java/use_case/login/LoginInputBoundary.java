444b83a7534151894cbb2e3532455bac9a4e5625
Pooja Mangra <poojamangra@gmail.com>
2024-11-27 16:40:10 -0500

    login use case done minus presenter and views and all that

--- src/main/java/use_case/login/LoginInputBoundary.java
+++ src/main/java/use_case/login/LoginInputBoundary.java
@@ -1,4 +1,10 @@
 package use_case.login;
 
 public interface LoginInputBoundary {
+    /**
+     * Executes the login use case.
+     * @param loginInputData the input data
+     */
+    void execute(LoginInputData loginInputData);
 }
+
--- src/main/java/use_case/login/LoginInteractor.java
+++ src/main/java/use_case/login/LoginInteractor.java
@@ -1,4 +1,46 @@
 package use_case.login;
 
-public class LoginInteractor {
+import data_access.InMemoryUserDAO;
+import entities.User;
+
+/**
+ * Log in interactor
+ */
+public class LoginInteractor implements LoginInputBoundary{
+    private final InMemoryUserDAO userDAO;
+    private final LoginOutputBoundary loginPresenter;
+
+    public LoginInteractor(InMemoryUserDAO userDAO, LoginOutputBoundary loginPresenter) {
+        this.userDAO = userDAO;
+        this.loginPresenter = loginPresenter;
+    }
+
+
+    /**
+     * Executes the login use case.
+     *
+     * @param loginInputData the input data
+     */
+    @Override
+    public void execute(LoginInputData loginInputData) {
+        final String username = loginInputData.getUsername();
+        final String password = loginInputData.getPassword();
+        if (!userDAO.userExists(username)) {
+            loginPresenter.prepareFailView(username + ": Account does not exist.");
+        }
+        else {
+            final String pwd = userDAO.getUser(username).getPassword();
+            if (!password.equals(pwd)) {
+                loginPresenter.prepareFailView("Incorrect password for \"" + username + "\".");
+            }
+            else {
+
+                final User user = userDAO.getUser(loginInputData.getUsername());
+
+                userDAO.setCurrentUser(user.getUsername());
+                final LoginOutputData loginOutputData = new LoginOutputData(user.getUsername());
+                loginPresenter.prepareSuccessView(loginOutputData);
+            }
+        }
+    }
 }
